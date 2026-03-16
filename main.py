db5ab962543e2b0ce7ff6386fdbcc53c83faa024
Poisonlocket <loribono@gmx.ch>
2026-03-16 16:13:22 +0100

    finished demo / ctf mini chall

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, HTTPException
 
 app = FastAPI()
 
+FLAG = "quack{0ne_c00k1e_pl31se}"
+
+# create flag file when container/app starts
+@app.on_event("startup")
+def create_flag():
+    with open("/flag.txt", "w") as f:
+        f.write(FLAG)
+
+
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Cookie dispenser Service for Ducks. Sadly we dont have enough funding yet to afford a Frontend Dev. Our Backend engineer also was kind of underpaid so we hope he did his job right and didnt leave any vulnerabilitys. Anyways, head to /cookie for a free cookie, if you dont get one, an admin needs to refill them at /admin"}
+    return {
+        "message": "Welcome to the Cookie dispenser Service for Ducks. Sadly we dont have enough funding yet to afford a Frontend Dev. Our Backend engineer also was kind of underpaid so we hope he did his job right and didnt leave any vulnerabilitys. Anyways, head to /cookie for a free cookie, if you dont get one, an admin needs to refill them at /admin"
+    }
+
 
 @app.get("/cookie")
-def cookie():
-    return {"message": "hemlo cookie"}
+def get_cookie(response: Response, username: str = "guest"):
+    response.set_cookie(
+        key="session",
+        value=username,
+        httponly=False
+    )
 
-@app.api_route("/admin")
-def admin():
-    return {"message": "yes"}
+    return {
+        "message": "Here is your cookie 🍪",
+        "username": username
+    }
+
 
 @app.get("/admin")
-def admin():
-    return {"message": "yes"}
+def admin(request: Request):
+    info = ""
+    session = request.cookies.get("session")
+
+    if not session:
+        raise HTTPException(status_code=401, detail="No cookie found")
+
+    username = session
+
+    try:
+        eval(username)
+    except:
+        info = "didnt get paid enough to handle this securely this code really makes me evaluate my choices"
+
+    return {
+        "message": "Welcome to the admin panel",
+        "username": username,
+        "admin_info": info
+    }
